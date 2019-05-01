366d12f18fae1488f8c33ab5a21ed38f1dd2d229
Qlancet <z464337866@163.com>
2019-05-01 15:03:33 +0800

    新添加一条test_code

--- tensqure_common/src/main/java/entity/StatusCode.java
+++ tensqure_common/src/main/java/entity/StatusCode.java
@@ -1,10 +1,11 @@
 package entity;
 
 public class StatusCode {
-    private static final int OK = 20000;//成功
+    public static final int OK = 20000;//成功
     private static final int LOGINERROR = 20002;//用户名或密码错误
     private static final int ERROR = 20001;//失败
     private static final int ACCESSERROR = 20003;//权限不足
     private static final int REMOTEERROR = 20004;//远程调用失败
     private static final int REPERROR = 20005;//重复操作
+    private static final int TEST_CODE= 0000;
 }
